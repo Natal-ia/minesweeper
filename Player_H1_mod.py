d8b4c0c93c17585113120bd7bc27380095532e29
Natalia Mejia <nataliamejia@javeriana.edu.co>
2024-04-23 22:49:26 -0500

    mejora

--- Player_H1_mod.py
+++ Player_H1_mod.py
@@ -3,6 +3,7 @@ import heapq
 from MineSweeperBoard import MineSweeperBoard
 
 class Player_H1:
+
     def __init__(self, w, h, m):
         self.m_Width = w
         self.m_Height = h
@@ -16,10 +17,66 @@ class Player_H1:
         while self.m_Probabilities:
             p, (i, j) = heapq.heappop(self.m_Probabilities)
             if not self.m_Mines[i][j] and self.m_Count[i][j] == 9:
-                return (i, j)
+                has_unmarked_neighbor = False
+                for x in range(-1, 2):
+                    for y in range(-1, 2):
+                        if x != 0 or y != 0:
+                            nx = i + x
+                            ny = j + y
+                            if 0 <= nx < self.m_Width and 0 <= ny < self.m_Height:
+                                if self.m_Count[nx][ny] == 9:
+                                    has_unmarked_neighbor = True
+                                    break
+                    if has_unmarked_neighbor:
+                        break
+                if has_unmarked_neighbor and self.m_Count[i][j] > 0:
+                    return (i, j)
+                elif not has_unmarked_neighbor:
+                    return (i, j)
+                # Si la celda contiene una mina, establecer su probabilidad en 1 y continuar
+                else:
+                    self.m_Probabilities.append((1.0, (i, j)))
         return None
 
     def update_cell(self, i, j):
+        if self.m_Count[i][j] == 1:
+            # Verificar si la celda está en una esquina y solo hay una celda no marcada en el vecindario
+            corner_cells = [(0, 0), (0, self.m_Height - 1), (self.m_Width - 1, 0), (self.m_Width - 1, self.m_Height - 1)]
+            if (i, j) in corner_cells:
+                unmarked_neighbors = 0
+                for x in [-1, 0, 1]:
+                    for y in [-1, 0, 1]:
+                        nx = i + x
+                        ny = j + y
+                        if (x != 0 or y != 0) and 0 <= nx < self.m_Width and 0 <= ny < self.m_Height:
+                            if self.m_Count[nx][ny] == 9:
+                                unmarked_neighbors += 1
+                if unmarked_neighbors == 1:
+                    for x in [-1, 0, 1]:
+                        for y in [-1, 0, 1]:
+                            nx = i + x
+                            ny = j + y
+                            if (x != 0 or y != 0) and 0 <= nx < self.m_Width and 0 <= ny < self.m_Height:
+                                if self.m_Count[nx][ny] == 9:
+                                    self.m_Mines[nx][ny] = True
+                                    self.m_NumberOfMines -= 1
+                                    return
+        elif 1 < self.m_Count[i][j] < 9:
+            # Verificar si hay x celdas no marcadas adyacentes y el número de minas restantes es igual a x
+            unmarked_neighbors = []
+            for x in [-1, 0, 1]:
+                for y in [-1, 0, 1]:
+                    nx = i + x
+                    ny = j + y
+                    if (x != 0 or y != 0) and 0 <= nx < self.m_Width and 0 <= ny < self.m_Height:
+                        if self.m_Count[nx][ny] == 9:
+                            unmarked_neighbors.append((nx, ny))
+            if len(unmarked_neighbors) == self.m_Count[i][j]:
+                for nx, ny in unmarked_neighbors:
+                    self.m_Mines[nx][ny] = True
+                    self.m_NumberOfMines -= 1
+                    return
+        # Actualización de celdas como antes
         m = 9
         n = 8
         for x in range(-1, 2):
@@ -80,6 +137,7 @@ if __name__ == "__main__":
         if next_play is not None:
             (i, j) = next_play
             print(board)
+            print("Current probabilities heap:", player.m_Probabilities)  # Imprimir el heap de probabilidades
             input('Next click on (' + chr(i + ord('A')) + ',' + chr(j + ord('A')) + ')')
             player.m_Play = (i, j)
             player.update(board.click(i, j))
